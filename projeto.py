db67f37e9093439282d01d48da3e9ec5d55861b7
Gabriella630 <mariagabriellaamarante630@gmail.com>
2025-07-11 13:33:52 +0000

    Confirmar

--- projeto.py
+++ projeto.py
@@ -1,1 +1,10 @@
-print('olá mundo')
+numero = 2
+print("os numeros pares ate 20:")
+
+while numero <= 20:
+    if numero % 2 == 0:
+        pares = pares + str(numero) + ","
+        nuero = numero + 2
+
+print("Numeros pares até 20:")
+print(pares)
